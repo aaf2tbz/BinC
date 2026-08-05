11af2e69be9bda456569723b3bdf791f1a415031
Avery Felts <aaf2tbz@users.noreply.github.com>
2026-08-04 23:52:13 -0600

    Make Pong motion blur smooth and theme rounds

--- examples/pong_host.m
+++ examples/pong_host.m
@@ -7,6 +7,8 @@
 #include "../binc/binc_runtime.h"
 #include "../binc/build/pong.h"
 #include <string.h>
+#include <math.h>
+#include <stdint.h>
 
 static BOOL g_up, g_down;
 static BOOL g_running=YES;
@@ -53,21 +55,21 @@ int main(void){
         NSButton *restartButton=[NSButton buttonWithTitle:@"RESTART ROUND" target:view action:@selector(restartGame:)]; restartButton.frame=NSMakeRect(412,285,200,42); restartButton.bezelStyle=NSBezelStyleRounded; restartButton.hidden=YES; [view addSubview:restartButton];
         layer.frame=view.bounds; [view setWantsLayer:YES]; view.layer=layer; layer.drawableSize=CGSizeMake(1024,768);
         NSError *err=nil;
-        MTLRenderPipelineDescriptor *pd=[MTLRenderPipelineDescriptor new]; pd.vertexFunction=[lib newFunctionWithName:@"pong_vs"]; pd.fragmentFunction=[lib newFunctionWithName:@"pong_fs"]; pd.colorAttachments[0].pixelFormat=layer.pixelFormat; pd.depthAttachmentPixelFormat=MTLPixelFormatDepth32Float;
+        MTLRenderPipelineDescriptor *pd=[MTLRenderPipelineDescriptor new]; pd.vertexFunction=[lib newFunctionWithName:@"pong_vs"]; pd.fragmentFunction=[lib newFunctionWithName:@"pong_fs"]; pd.colorAttachments[0].pixelFormat=layer.pixelFormat; pd.colorAttachments[0].blendingEnabled=YES; pd.colorAttachments[0].sourceRGBBlendFactor=MTLBlendFactorSourceAlpha; pd.colorAttachments[0].destinationRGBBlendFactor=MTLBlendFactorOneMinusSourceAlpha; pd.colorAttachments[0].sourceAlphaBlendFactor=MTLBlendFactorOne; pd.colorAttachments[0].destinationAlphaBlendFactor=MTLBlendFactorOneMinusSourceAlpha; pd.depthAttachmentPixelFormat=MTLPixelFormatDepth32Float;
         id<MTLRenderPipelineState> pipe=[dev newRenderPipelineStateWithDescriptor:pd error:&err]; if(!pipe){ NSLog(@"BinC render pipeline: %@",err); return 1; }
         MTLDepthStencilDescriptor *dd=[MTLDepthStencilDescriptor new]; dd.depthCompareFunction=MTLCompareFunctionLessEqual; dd.depthWriteEnabled=YES; id<MTLDepthStencilState> depth=[dev newDepthStencilStateWithDescriptor:dd];
-        HostGame game={0,0,0.1875f,0.5f,0.25f,0,0,0,0,0,30.0f,1,3,0,0,0,0,0}; HostGame initialGame=game; float vertices[378*4]={0};
+        HostGame game={0,0,0.1875f,0.75f,0.375f,0,0,0,0,0,30.0f,1,3,0,0,0,0,0}; HostGame initialGame=game; float vertices[378*4]={0};
         BincBuffer *gb=binc_buffer_new(rt,&game,sizeof game); BincBuffer *vb=binc_buffer_new(rt,vertices,sizeof vertices);
         NSURL *musicURL=[NSURL fileURLWithPath:@"../examples/pong_music.wav"]; AVAudioPlayer *music=[[AVAudioPlayer alloc] initWithContentsOfURL:musicURL error:&err];
         AVAudioPlayer *hit=[[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:@"../examples/pong_hit.wav"] error:&err];
         AVAudioPlayer *life=[[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:@"../examples/pong_life.wav"] error:&err];
         AVAudioPlayer *score=[[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:@"../examples/pong_score.wav"] error:&err];
-        if(music){ music.numberOfLoops=-1; [music setVolume:0.35]; [music prepareToPlay]; [music play]; } else NSLog(@"BinC Pong music unavailable: %@",err);
+        if(music){ music.numberOfLoops=-1; music.enableRate=YES; music.rate=1.0; [music setVolume:0.35]; [music prepareToPlay]; [music play]; } else NSLog(@"BinC Pong music unavailable: %@",err);
         if(hit)hit.volume=0.55; if(life)life.volume=0.7; if(score)score.volume=0.7;
-        __block BOOL gameOver=NO; __block int lastEvent=0; __weak PongView *weakView=view;
+        __block BOOL gameOver=NO; __block int lastEvent=0; __block int lastRound=1; __weak PongView *weakView=view;
         view.restartHandler=^{
             memcpy(binc_buffer_contents(gb), &initialGame, sizeof initialGame); memset(binc_buffer_contents(vb),0,sizeof vertices);
-            gameOver=NO; lastEvent=0; gameOverLabel.hidden=YES; restartButton.hidden=YES; [weakView.window makeFirstResponder:weakView];
+            gameOver=NO; lastEvent=0; lastRound=1; music.currentTime=0; music.rate=1.0; music.volume=0.35; [music play]; gameOverLabel.hidden=YES; restartButton.hidden=YES; [weakView.window makeFirstResponder:weakView];
         };
         id<MTLTexture> depthTex=nil;
         while(g_running && win.isVisible){ @autoreleasepool {
@@ -82,6 +84,9 @@ int main(void){
             int seconds=(int)state->round_time; if(seconds<0)seconds=0;
             win.title=[NSString stringWithFormat:@"BinC // NEON PONG   ROUND %d   TIME %02d   P %d : AI %d   LIVES %d",state->round,seconds,state->player_score,state->ai_score,state->lives];
             if(state->event!=lastEvent){ if(state->event==1){ hit.currentTime=0; [hit play]; } if(state->event==2){ life.currentTime=0; [life play]; } if(state->event==3){ score.currentTime=0; [score play]; } lastEvent=state->event; }
+            if(state->round!=lastRound && state->round>0){
+                lastRound=state->round; if(music){ music.rate=MIN(1.75,1.0+0.04*(state->round-1)); music.volume=MIN(0.8,0.35+0.025*(state->round-1)); if(music.duration>4){ uint32_t slot=arc4random_uniform(1000); music.currentTime=fmod((double)slot/1000.0*(music.duration-2.0),music.duration-2.0); } [music play]; }
+            }
             if(!gameOver && state->game_over){
                 gameOver=YES; gameOverLabel.stringValue=state->game_over==2?@"CONGRATS, YOU WON!":@"YOU LOST, TRY AGAIN"; gameOverLabel.hidden=NO; restartButton.hidden=NO;
             }
